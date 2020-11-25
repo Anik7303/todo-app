f9868e507146b6643c32c384511e3eca7f2a790f
Anik7303 <anik7303@gmail.com>
2020-11-26 01:08:36 +0600

    fixed axios.js for production build

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: process.env.NODE_ENV === "production" ? "" : "http://localhost:5000",
+    baseURL:
+        process.env.NODE_ENV === "production"
+            ? "https://todo-app-9i4gle5a.herokuapp.com"
+            : "http://localhost:5000",
 });
